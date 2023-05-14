8d8bf7e2d148e8294d49ac5179dbb09c0878e959
Kristen <105521957+kristenwcheung@users.noreply.github.com>
2023-05-14 16:48:22 -0400

    removed ec2 instance and variable

--- main.tf
+++ main.tf
@@ -1,8 +1,1 @@
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorldAgain"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,1 @@
-variable "ami" {
-  type = string
-}
+
